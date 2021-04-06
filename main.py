37fb5e56d0006b28003f31030991cd53b0f83ecd
flashz7 <jamescolbertv@gmail.com>
2021-04-06 04:11:07 -0700

    modified main.py to store data in JSON format. Also added exception handling features

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from pyperclip import copy
 from random import choice, randint, shuffle
 from tkinter import *
 from tkinter import messagebox
+import json
 
 DEFAULT_USER = 'jamescolbertv@gmail.com'
 
@@ -30,24 +31,47 @@ def save_password():
     website = website_input.get()
     username = username_input.get()
     password = password_input.get()
+    new_data = {
+        website: {
+            "username": username,
+            "password": password,
+        }
+    }
 
     if len(website) == 0 or len(password) == 0:
         messagebox.showerror(title="Oops", message="Please don't leave any fields empty!")
     else:
-        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {username} "
-                                                              f"\nPassword: {password} \nIs it ok to save?")
-
-        if is_ok:
-            with open("./data.txt", "a") as f:
-                f.write(f"{website} | {username} | {password}\n")
+        try:
+            with open("data.json", "r") as f:
+                data = json.load(f)
+        except FileNotFoundError:
+            with open("data.json", "w") as f:
+                json.dump(new_data, f, indent=4)
+        else:
+            data.update(new_data)
+            with open("data.json", "w") as f:
+                json.dump(data, f, indent=4)
+        finally:
             website_input.delete(0, 'end')
             password_input.delete(0, 'end')
-            if username != DEFAULT_USER:
-                username_input.delete(0, 'end')
-                username_input.insert(0, DEFAULT_USER)
-            password_input.config(show="*")
+        if username != DEFAULT_USER:
+            username_input.delete(0, 'end')
+            username_input.insert(0, DEFAULT_USER)
+        password_input.config(show="*")
 
-
+# ---------------------------- FIND PASSWORD ------------------------------- #
+def find_password():
+    website = website_input.get()
+    try:
+        with open("data.json", "r") as f:
+            data = json.load(f)
+    except FileNotFoundError:
+        messagebox.showerror(title="ERROR", message="No Data File Found")
+    else:
+        if website in data:
+            messagebox.showinfo(title="Entry found!", message=f"Password found!\n{data[website]['username']}\n{data[website]['password']}")
+        else:
+            messagebox.showinfo(title="Not found", message="No details for the website exist.")
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager")
@@ -61,9 +85,12 @@ canvas.grid(column=1, row=0)
 website_label = Label(text="Website:")
 website_label.grid(column=0, row=1)
 
-website_input = Entry(width=35)
+website_input = Entry(width=21)
 website_input.focus()
-website_input.grid(column=1, row=1, columnspan=2)
+website_input.grid(column=1, row=1)
+
+search_button = Button(width=13, text="Search", command=find_password)
+search_button.grid(column=2, row=1)
 
 username_label = Label(text="Username:")
 username_label.grid(column=0, row=2)
